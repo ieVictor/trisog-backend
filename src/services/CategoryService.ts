01b89b89ab6af46cbff11ac9fcdee67a18ca0060
Victor Gabriel <victor_tiss@outlook.com>
2024-08-27 17:50:54 -0300

    refactor(category): fix return on sucess

--- src/services/CategoryService.ts
+++ src/services/CategoryService.ts
@@ -5,7 +5,7 @@ export class CategoryService {
   static async getCategories(): Promise<{ categories: Category[] | null, error: string | null }> {
     try {
       const categories = await prismaClient.category.findMany();
-      return { categories, error: 'null' }
+      return { categories, error: null }
     } catch (error) {
       console.error('Error retrieving categories: ', error);
       return { categories: null, error: 'Internal server error' };
