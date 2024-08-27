880bfd16b6a31a2bf490d11ce9005ecbc34f88df
Victor Gabriel <victor_tiss@outlook.com>
2024-08-27 17:19:17 -0300

    feat(tour): add tour service implementation

--- src/services/TourService.ts
+++ src/services/TourService.ts
@@ -1,10 +1,21 @@
 import { Tour, TourInsert } from "../models/tourModel";
 import prismaClient from "../utils/database";
 
+export type ToursResponse = { tours: Tour[] | null, error: string | null }
+type TourResponse = { tour: Tour | null, error: string | null };
+
 export class TourService {
-  static async getTours(): Promise<{ tours: Tour[] | null, error: string | null }> {
+  static async getTours(): Promise<ToursResponse> {
     try {
-      const tours = await prismaClient.tour.findMany();
+      const tours = await prismaClient.tour.findMany({
+        include: {
+          categories: {
+            include: {
+              category: true
+            }
+          }
+        }
+      });
       return { tours, error: null}
     } catch (error) {
       console.error('Error retrieving tours: ', error);
@@ -12,12 +23,42 @@ export class TourService {
     }
   }
 
-  static async getTourById(tour_id: string): Promise<{ tour: Tour | null, error: string | null}> {
+  static async getToursByCategories(categories: number[]): Promise<ToursResponse> {
+    try {
+      const tours = await prismaClient.tour.findMany({
+        where: {
+          categories: {
+            some: {
+              categoryId: { in: categories }
+            }
+          }
+        },
+        include: {
+          categories: {
+            include: { category: true }
+          }
+        }
+      })
+      return { tours, error: null}
+    } catch (error) {
+      console.error('Error retrieving tours: ', error);
+      return { tours: null, error: 'Interal server error'}
+    }
+  }
+
+  static async getTourById(tour_id: string): Promise<TourResponse> {
     try {
       const tour = await prismaClient.tour.findUnique({
         where: {
           id: tour_id
         },
+        include: {
+          categories: {
+            include: {
+              category: true
+            }
+          } 
+        }
       });
 
       if (tour) return { tour, error: null};
@@ -29,12 +70,20 @@ export class TourService {
   };
 
   static async createTour(data: TourInsert): Promise<{ createdTourId: string | null; error: string | null }> {
+    const {categoriesIds, ...tourdata} = data
     const initialRating = 0
     try {
       const result = await prismaClient.tour.create({
         data: {
-          ...data,
-          rating: initialRating
+          ...tourdata,
+          rating: initialRating,
+          categories: {
+            create: data.categoriesIds.map(categoryId => ({
+              category: {
+                connect: { id: categoryId }
+              }
+            }))
+          }
         },
       })
       return { createdTourId: result.id, error: null}
