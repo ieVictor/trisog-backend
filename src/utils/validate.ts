92c76cabdc3f7bc6f7cd90a081777822d246c3a6
Victor Gabriel <victor_tiss@outlook.com>
2024-08-30 09:14:32 -0300

    feat(review): add uuid verifier

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -2,3 +2,8 @@ export function isValidFirebaseUID(uid: string): boolean {
   const uidRegex = /^[A-Za-z0-9-_]{28,256}$/;
   return uidRegex.test(uid);
 }
+
+export function isValidUUID(id: string): boolean {
+  const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+  return uuidRegex.test(id);
+}
