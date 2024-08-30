04d2b90dc4a7cd6adc020f73cd50f0e64131ac70
Victor Gabriel <victor_tiss@outlook.com>
2024-08-30 09:13:36 -0300

    feat(review): improve model validations

--- src/models/reviewModel.ts
+++ src/models/reviewModel.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString, IsNumber, IsDateString } from "class-validator";
+import { IsNotEmpty, IsString, IsNumber, IsDateString, isBoolean, IsBoolean, Min, Matches } from "class-validator";
 
 class Review {
   @IsString({ message: "The id field must be a string" })
@@ -69,44 +69,65 @@ class ReviewInsert {
   @IsNotEmpty({ message: "The user id field is mandatory" })
   userId: string;
 
-  @IsString({ message: "The overview field must be a string" })
-  @IsNotEmpty({ message: "The overview field is mandatory" })
+  @IsString({ message: "The email field must be a string" })
+  @IsNotEmpty({ message: "The email field mandatory" })
+  creatorEmail: string;
+
+  @IsString({ message: "The creator name field must be a string" })
+  @IsNotEmpty({ message: "The review creator name is mandatory" })
+  @Matches(/^[A-Za-zÀ-ÖØ-öø-ÿ'\- ]{2,}$/, { message: 'The creator name must not contain numbers'})
+  creatorName: string;
+
+  @IsString({ message: "The comment field must be a string" })
+  @IsNotEmpty({ message: "The comment field is mandatory" })
   overview: string;
 
   @IsNumber({}, { message: "The services field must be a number" })
   @IsNotEmpty({ message: "The services field is mandatory" })
+  @Min(1, { message: "The services field must be greater than 0" })
   services: number;
 
   @IsNumber({}, { message: "The locations field must be a number" })
   @IsNotEmpty({ message: "The locations field is mandatory" })
+  @Min(1, { message: "The locations field must be greater than 0" })
   locations: number;
 
   @IsNumber({}, { message: "The amenities field must be a number" })
   @IsNotEmpty({ message: "The amenities field is mandatory" })
+  @Min(1, { message: "The amenities field must be greater than 0" })
   amenities: number;
 
   @IsNumber({}, { message: "The prices field must be a number" })
   @IsNotEmpty({ message: "The prices field is mandatory" })
+  @Min(1, { message: "The prices field must be greater than 0" })
   prices: number;
 
   @IsNumber({}, { message: "The food field must be a number" })
   @IsNotEmpty({ message: "The food field is mandatory" })
+  @Min(1, { message: "The food field must be greater than 0" })
   food: number;
 
   @IsNumber({}, { message: "The room field must be a number" })
   @IsNotEmpty({ message: "The room field is mandatory" })
+  @Min(1, { message: "The room field must be greater than 0" })
   room: number;
+
+  @IsBoolean({message: 'The anonymous flag must be a boolean'})
+  anonymous: boolean;
 
   constructor(payload: ReviewInsert) {
     this.tourId = typeof payload.tourId === "string" ? payload.tourId.trim() : payload.tourId;
     this.userId = typeof payload.userId === "string" ? payload.userId.trim() : payload.userId;
     this.overview = typeof payload.overview === "string" ? payload.overview.trim() : payload.overview;
+    this.creatorEmail = typeof payload.creatorEmail === "string" ? payload.creatorEmail.trim() : payload.creatorEmail;
+    this.creatorName = typeof payload.creatorName === "string" ? payload.creatorName.trim() : payload.creatorName;
     this.services = payload.services;
     this.locations = payload.locations;
     this.amenities = payload.amenities;
     this.prices = payload.prices;
     this.food = payload.food;
     this.room = payload.room;
+    this.anonymous = payload.anonymous
   }
 }
 
