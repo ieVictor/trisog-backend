5010c1f32c12fcaf3c558295289adfcc7763f75b
Victor Gabriel <victor_tiss@outlook.com>
2024-08-30 09:32:12 -0300

    feat(review): add anonymous option & delete controller

--- src/controllers/reviewController.ts
+++ src/controllers/reviewController.ts
@@ -2,6 +2,7 @@ import { validate } from "class-validator";
 import { ReviewInsert } from "../models/reviewModel";
 import { ReviewService } from "../services/ReviewService";
 import { Request, Response } from 'express';
+import { isValidUUID } from "../utils/validate";
 
 export class ReviewController {
   static async getReviews(req: Request, res: Response): Promise<Response> {
@@ -22,7 +23,10 @@ export class ReviewController {
   }
 
   static async createReview(req: Request, res: Response): Promise<Response> {
+    const anonymous = req.query.anonymous === 'true';
     const payload = new ReviewInsert(req.body);
+    payload.anonymous = anonymous
+
     const errors = await validate(payload);
     if (errors.length > 0) {
       const firstError = errors[0];
@@ -34,10 +38,18 @@ export class ReviewController {
     if (createTourError) return res.status(500).json({ msg: createTourError });
     if (!createdReviewId || createdReviewId === '') return res.status(404).json({ msg: 'No data found'});
 
-    const { reviews, error: getReviewError } = await ReviewService.getReviews();
+    const { review, error: getReviewError } = await ReviewService.getReviewById(createdReviewId);
     if (getReviewError) return res.status(500).json({ msg: getReviewError });
-    if (!reviews) return res.status(404).json({ msg: 'No data found' });
+    if (!review) return res.status(404).json({ msg: 'No data found' });
 
-    return res.status(201).json(reviews)
+    return res.status(201).json(review);
+  }
+
+  static async deleteReview(req: Request, res: Response): Promise<Response> {
+    const reviewId = req.params.review_id
+    if (!isValidUUID(reviewId)) return res.status(400).json({ msg: 'Provide a valid review id to be deleted'});
+    const { deletedReview, error: deletedReviewError } = await ReviewService.deleteReview(reviewId);
+    if (!deletedReview) return res.status(500).json({ msg: deletedReviewError });
+    return res.status(200).json({ msg: 'Successfully delete!' });
   }
 }
